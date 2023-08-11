02151665bc562a91d16244fa6f545e6811494c66
swatii7 <swatic946@gmail.com>
2023-08-11 17:37:26 +0530

    do some validation on input fields

--- src/component/Checkout/Checkout.jsx
+++ src/component/Checkout/Checkout.jsx
@@ -3,18 +3,28 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CustomInput from './CustomInput/CustomInput';
-import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
+import { CountryDropdown, RegionDropdown} from 'react-country-region-selector';
 import poweredImg from '../../assets/Checkout/powered_by_stripe.png'
 import {LockFill} from 'react-bootstrap-icons'
 import CheckoutFooter from './CheckoutFooter/CheckoutFooter';
+import { SnackbarProvider, enqueueSnackbar } from 'notistack';
 
 
 function Checkout({forwardRef}) {
 
-  // const details = [
-  //   first_name = 'First Name',
+  const [state, setState] = useState({
+    first_name:'',
+    last_name: '',
+    email_address: '',
+    mobile_number: '',
+    shipping_address: '',
+    appartment: '',
+    city_name:'',
+    zip_code: '',
+    card_number: '',
+    isDisabled: true,
 
-  // ]
+  })
 
   const [country, setCountry] = useState('');
   const [region, setRegion] = useState('');
@@ -27,7 +37,30 @@ function Checkout({forwardRef}) {
     setRegion(val);
   };
 
+  function buttonHandler(){
+    console.log('Clicked')
+   if(state.first_name ===''||
+   state.last_name === ''||
+   state.email_address === ''||
+   state.shipping_address === ''||
+   state.city_name ===''||
+   state.zip_code === ''||
+   state.card_number === ''){
+    
+    enqueueSnackbar('All fields are required', {variant: 'error'})
+   }
+   else{
+    // state.isDisabled(false)
+    console.log('abled')
+   }
+  }
+
+  function inputHanlder(e){
+    setState(e.target.value)
+  }
+
     return (
+      <SnackbarProvider>
         <div  ref={forwardRef} className='checkoutContainer'>
            <Container>
       <Row>
@@ -50,14 +83,14 @@ function Checkout({forwardRef}) {
     <Container>
       <Row>
         <Col>
-        <CustomInput lg= {6} sm= {6} xs= {6} type='text' name='First Name' placeholder='First Name' />
+        <CustomInput lg= {6} sm= {6} xs= {6} type='text' placeholder='First Name' value= {state.first_name} onchange={inputHanlder} />
         </Col>
         <Col>
-        <CustomInput lg= {6} sm= {6} xs= {6} placeholder= 'Last Name' type='text' />
+        <CustomInput lg= {6} sm= {6} xs= {6} placeholder= 'Last Name' value= {state.last_name} type='text' onchange={inputHanlder} />
         </Col>
       </Row>
-      <CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Email Address' type='text' />
-      <CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Phone Number(Optional)' type='number' />
+      <CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Email Address' type='text' value={state.email_address} onchange={inputHanlder} />
+      <CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Phone Number(Optional)' type='number' onchange={inputHanlder} />
       <div className='d-flex mb-40'>
         <input type='checkbox' style={{width: '17px', transform: 'scale(1.2)'}} />
         <p className='px-3 pt-3'>Keep me up to date on news and exclusive offers</p>
@@ -70,10 +103,10 @@ function Checkout({forwardRef}) {
   Where are we sending this to?
   </p>
 </div>
-<CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Shipping Address' type='text' />
-<CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Apartment, Suite, etc.(Optional)' type='text' />
+<CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Shipping Address' type='text' value={state.shipping_address} onchange={inputHanlder} />
+<CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Apartment, Suite, etc.(Optional)' type='text' onchange={inputHanlder} />
 <div className='d-flex' style={{ justifyContent: "space-between" }}>
-<CustomInput lg= {6} sm= {6} xs= {6} placeholder= 'Town/City' type='text' />
+<CustomInput lg= {6} sm= {6} xs= {6} placeholder= 'Town/City' type='text' value={state.city_name} onchange={inputHanlder} />
 <RegionDropdown
         country={country}
         name='none'
@@ -84,7 +117,7 @@ function Checkout({forwardRef}) {
       
 </div>
 <div className='d-flex' style={{ justifyContent: "space-between" }}>
-<CustomInput lg= {6} sm= {6} xs= {6} placeholder= 'Post/Zip Code' type='text' />
+<CustomInput lg= {6} sm= {6} xs= {6} placeholder= 'Post/Zip Code' type='text' onchange={inputHanlder} />
       <CountryDropdown
         value={country}
         onChange={(val) => selectCountry(val)}
@@ -106,7 +139,7 @@ function Checkout({forwardRef}) {
   </div>
 </div>
 <div className='mb-10'>
-<CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Card Number' />
+<CustomInput lg= {12} sm= {6} xs= {6} placeholder= 'Card Number' onchange={inputHanlder} />
 </div>
 <div className='formHeader'>
       <h5 className='fw-700 Cooper fs-28 pt-1'>
@@ -118,7 +151,7 @@ function Checkout({forwardRef}) {
       </div>
       <div className='d-flex'>
         <div className= 'discInput'>
-        <CustomInput lg= {9} sm= {6} xs= {6} placeholder= 'Discount Code' />
+        <CustomInput lg= {9} sm= {6} xs= {6} placeholder= 'Discount Code' onchange={inputHanlder} />
         </div>
       
       <button className='discountBtn opacity'>Apply</button>
@@ -140,10 +173,16 @@ function Checkout({forwardRef}) {
       <p>$44.00</p>
     </div>
    </div>
-   <button className='purchaseButton opacity d-flex'>
+   {/* {state.isDisabled === true?<button className='purchaseButton d-flex' onClick={buttonHandler} disabled={true} >
+   <LockFill className='lockIcon' />
+   <p className='fs-24 fw-bold Quicksand' style={{paddingLeft: '16px'}}>Complete Purchase</p>
+   </button>: */}
+   <button className='purchaseButton d-flex' onClick={buttonHandler}>
    <LockFill className='lockIcon' />
    <p className='fs-24 fw-bold Quicksand' style={{paddingLeft: '16px'}}>Complete Purchase</p>
    </button>
+   {/* } */}
+   
    <div className='fs-18 transPara Quicksand'>
    <p>All transactions are safe & secured by 2048 bit SSL encryption.</p>
    </div>
@@ -157,6 +196,7 @@ function Checkout({forwardRef}) {
     </Container>
     <CheckoutFooter />
         </div>
+        </SnackbarProvider>
     )
 }
 
